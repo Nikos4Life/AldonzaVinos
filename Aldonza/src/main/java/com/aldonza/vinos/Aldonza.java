273e1322730cf0ba49d15248422654844aacf076
Miguel <mmmx3m@gmail.com>
2014-03-04 15:01:42 +0100

    Added:
               - ViewPager into ViewPager

--- Aldonza/src/main/java/com/aldonza/vinos/Aldonza.java
+++ Aldonza/src/main/java/com/aldonza/vinos/Aldonza.java
@@ -1,33 +1,52 @@
 package com.aldonza.vinos;
 
 
-import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
- * Created by Miguel on 1/02/14.
+ * This shows how to add a map to a ViewPager. Note the use of
+ * {@link android.view.ViewGroup#requestTransparentRegion(android.view.View)} to reduce jankiness.
  */
-public class Aldonza extends Activity {
+public class Aldonza extends FragmentActivity {
+    private MyAdapter mAdapter;
+    private ViewPager mPager;
 
+
+    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.aldonza_screen);
-        
-        MyPageAdapterPremios adapter = new MyPageAdapterPremios();
-		ViewPager viewPager = (ViewPager) findViewById(R.id.viewPagerA);
-		viewPager.setAdapter(adapter);
+        mAdapter = new MyAdapter(getSupportFragmentManager());
 
-		//---starts with the second page---
-		viewPager.setCurrentItem(0);
-		
+        mPager = (ViewPager) findViewById(R.id.viewPagerAldonza);
+        mPager.setAdapter(mAdapter);
+        mPager.setPageTransformer(true, new ZoomOutPageTransformer());
+
+        // This is required to avoid a black flash when the map is loaded. The
+        // flash is due
+        // to the use of a SurfaceView as the underlying view of the map.
+        mPager.requestTransparentRegion(mPager);
     }
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -37,6 +56,7 @@ public class Aldonza extends Activity {
 
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.about) {
+
             Class<?> clase = null;
             try {
                 clase = Class.forName("com.aldonza.vinos.Dialog");
@@ -48,6 +68,7 @@ public class Aldonza extends Activity {
             }
         }
         if (item.getItemId() == R.id.Preferencias) {
+
             Class<?> clase = null;
             try {
                 clase = Class.forName("com.aldonza.vinos.MuestraLasPreferencias");
@@ -59,6 +80,7 @@ public class Aldonza extends Activity {
 
         }
         if (item.getItemId() == R.id.cocerCita) {
+
             Class<?> clase = null;
             try {
                 clase = Class.forName("com.aldonza.vinos.ConcertarCita");
@@ -72,35 +94,142 @@ public class Aldonza extends Activity {
         return false;
 
     }
+    /** A simple fragment that displays a TextView. */
+    public static class aSeleccion extends Fragment {
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle bundle) {
+            return inflater.inflate(R.layout.aldonzaseleccion, container,
+                    false);
+        }
+        @Override
+        public void onViewCreated(View view, Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            MyPageAdapterPremios adapter = new MyPageAdapterPremios();
+            ViewPager mViewPager = (ViewPager) view.findViewById(R.id.viewPagerPremios);
+            mViewPager.setAdapter(adapter);
+        }
+    }
+
+    /** A simple fragment that displays a TextView. */
+    public static class Elaboracion extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle bundle) {
+            View v = inflater.inflate(R.layout.bodegaelaboracion, container,
+                    false);
+            return v;
+        }
+    }
+
+    public static class Mapa extends Fragment {
+        private static final int MAP_TYPE_SATELLITE = 2;
+        final LatLng HAMBURG = new LatLng(53.558, 9.927);
+        final LatLng KIEL = new LatLng(53.551, 9.993);
+        GoogleMap map;
+
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View v = inflater.inflate(R.layout.bodegaubicacion, null, false);
+//			FragmentTransaction ft = getFragmentManager().beginTransaction();
+//
+//			map = ((SupportMapFragment) getFragmentManager().findFragmentById(
+//					R.id.map)).getMap();
+//
+//
+//			ft.commit();
+//
+//			Marker hamburg = map.addMarker(new MarkerOptions()
+//					.position(HAMBURG).title("Hamburg"));
+//			/*
+//			 * Marker kiel = map.addMarker(new MarkerOptions() .position(KIEL)
+//			 * .title("Kiel") .snippet("Kiel is cool")
+//			 * .icon(BitmapDescriptorFactory
+//			 * .fromResource(R.drawable.ic_launcher)));
+//			 */
+//			// Move the camera instantly to hamburg with a zoom of 15.
+//			map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
+//
+//			// Zoom in, animating the camera.
+//			map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+//			*/
+            map = ((SupportMapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+            map.setMapType(MAP_TYPE_SATELLITE);
+
+            LatLng Aldonza = new LatLng(38.9943, -2.41438);
+
+            map.setMyLocationEnabled(true);
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(Aldonza, 13));
+
+            map.addMarker(new MarkerOptions()
+                    .title("Aldonza")
+                    .snippet("Vinos")
+                    .position(Aldonza).icon(BitmapDescriptorFactory.fromResource(R.drawable.marker)));
+            return v;
+        }
+
+        public void onActivityCreated(Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
+
+        }
+    }
+
+    /**
+     * A simple FragmentPagerAdapter that returns two TextFragment and a
+     * SupportMapFragment.
+     */
+    public static class MyAdapter extends FragmentPagerAdapter {
+        public MyAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new aSeleccion();
+                case 1:
+                    return new Elaboracion();
+                case 2:
+                    return new Mapa();
+                default:
+                    return null;
+            }
+        }
+    }
     public void twitter(View v){
-    	Intent intent = new Intent(Intent.ACTION_VIEW);
-    	intent.setData(Uri.parse("https://twitter.com/AldonzaWines"));
-    	startActivity(intent);
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://twitter.com/AldonzaWines"));
+        startActivity(intent);
     }
     public void facebook(View v){
-    	Intent intent = new Intent(Intent.ACTION_VIEW);
-    	intent.setData(Uri.parse("https://www.facebook.com/aldonzawines"));
-    	startActivity(intent);
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://www.facebook.com/aldonzawines"));
+        startActivity(intent);
     }
     public void youtube(View v){
-//    	Intent intent = new Intent(Intent.ACTION_VIEW);
-//    	intent.setData(Uri.parse("https://www.youtube.com/channel/UCbgjP5f05GQqAeg8b2O5xMQ"));
-//    	startActivity(intent);
-    	
-    	Intent intent = new Intent(Intent.ACTION_SEARCH);
-    	intent.setPackage("com.google.android.youtube");
-    	//la cadena a buscar
-    	intent.putExtra("query", "una ranita iba caminando");
-    	intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-    	try
-    	{
-    	  startActivity(intent);
-    	} 
-    	catch (ActivityNotFoundException e)
-    	{
-    	  //la app no esta instalada, mostrar por ejemplo un AlertDialog
-    	}
-    }
-   
+//	    	Intent intent = new Intent(Intent.ACTION_VIEW);
+//	    	intent.setData(Uri.parse("https://www.youtube.com/channel/UCbgjP5f05GQqAeg8b2O5xMQ"));
+//	    	startActivity(intent);
 
+        Intent intent = new Intent(Intent.ACTION_SEARCH);
+        intent.setPackage("com.google.android.youtube");
+        //la cadena a buscar
+        intent.putExtra("query", "una ranita iba caminando");
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        try
+        {
+            startActivity(intent);
+        }
+        catch (ActivityNotFoundException e)
+        {
+            //la app no est� instalada, mostrar por ejemplo un AlertDialog
+        }
+    }
 }
