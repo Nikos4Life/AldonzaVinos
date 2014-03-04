f5c7e46109d490ab2e6b31f316d282e1d2990963
Garx5 <alberto_25_5_94@hotmail.com>
2014-03-04 16:44:45 +0100

    aldonzaPlaces(mitad)

--- Aldonza/src/main/java/com/aldonza/vinos/AldonzaPlaces.java
+++ Aldonza/src/main/java/com/aldonza/vinos/AldonzaPlaces.java
@@ -1,24 +1,45 @@
 package com.aldonza.vinos;
 
 
-import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
+import android.view.ViewGroup;
+
 
 /**
- * Created by Miguel on 6/02/14.
+ * This shows how to add a map to a ViewPager. Note the use of
+ * {@link android.view.ViewGroup#requestTransparentRegion(android.view.View)} to reduce jankiness.
  */
-public class AldonzaPlaces extends Activity {
+public class AldonzaPlaces extends FragmentActivity {
+    private MyAdapter mAdapter;
+    private ViewPager mPager;
+
+    /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle bundle) {
-        super.onCreate(bundle);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.aldonza_places);
-        
+        mAdapter = new MyAdapter(getSupportFragmentManager());
+
+        mPager = (ViewPager) findViewById(R.id.viewPagerPlaces);
+        mPager.setAdapter(mAdapter);
+        mPager.setPageTransformer(true, new ZoomOutPageTransformer());
+
+        // This is required to avoid a black flash when the map is loaded. The
+        // flash is due
+        // to the use of a SurfaceView as the underlying view of the map.
+        mPager.requestTransparentRegion(mPager);
     }
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -28,7 +49,7 @@ public class AldonzaPlaces extends Activity {
 
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.about) {
-            
+
             Class<?> clase = null;
             try {
                 clase = Class.forName("com.aldonza.vinos.Dialog");
@@ -40,7 +61,7 @@ public class AldonzaPlaces extends Activity {
             }
         }
         if (item.getItemId() == R.id.Preferencias) {
-            
+
             Class<?> clase = null;
             try {
                 clase = Class.forName("com.aldonza.vinos.MuestraLasPreferencias");
@@ -52,7 +73,7 @@ public class AldonzaPlaces extends Activity {
 
         }
         if (item.getItemId() == R.id.cocerCita) {
-            
+
             Class<?> clase = null;
             try {
                 clase = Class.forName("com.aldonza.vinos.ConcertarCita");
@@ -66,11 +87,81 @@ public class AldonzaPlaces extends Activity {
         return false;
 
     }
-    public void mapa(View v){
-    	
-    	Intent i = new Intent(Intent.ACTION_VIEW,Uri.parse("geo:40.37367,-3.919848?q=40.37367,-3.919848"));
-    			startActivity(i);
-
+    /** A simple fragment that displays a TextView. */
+    public static class BodasCamacho extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle bundle) {
+            return inflater.inflate(R.layout.bodascamacho, container,
+                    false);
+        }
     }
-    
+
+    /** A simple fragment that displays a TextView. */
+    public static class Arqueologico extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle bundle) {
+            View v = inflater.inflate(R.layout.arqueologico, container,
+                    false);
+            return v;
+        }
+    }
+
+
+    /**
+     * A simple FragmentPagerAdapter that returns two TextFragment and a
+     * SupportMapFragment.
+     */
+    public static class MyAdapter extends FragmentPagerAdapter {
+        public MyAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new BodasCamacho();
+                case 1:
+                    return new Arqueologico();
+                default:
+                    return null;
+            }
+        }
+    }
+    public void twitter(View v){
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://twitter.com/AldonzaWines"));
+        startActivity(intent);
+    }
+    public void facebook(View v){
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://www.facebook.com/aldonzawines"));
+        startActivity(intent);
+    }
+    public void youtube(View v){
+//	    	Intent intent = new Intent(Intent.ACTION_VIEW);
+//	    	intent.setData(Uri.parse("https://www.youtube.com/channel/UCbgjP5f05GQqAeg8b2O5xMQ"));
+//	    	startActivity(intent);
+
+        Intent intent = new Intent(Intent.ACTION_SEARCH);
+        intent.setPackage("com.google.android.youtube");
+        //la cadena a buscar
+        intent.putExtra("query", "una ranita iba caminando");
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        try
+        {
+            startActivity(intent);
+        }
+        catch (ActivityNotFoundException e)
+        {
+            //la app no est� instalada, mostrar por ejemplo un AlertDialog
+        }
+    }
 }
