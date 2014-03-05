a3c56c87fe17bf74e0ba82c0c871cb1b6977f12f
Miguel <mmmx3m@gmail.com>
2014-03-06 00:42:03 +0100

    Fixed:
               - Fixed layouts

--- Aldonza/src/main/java/com/aldonza/vinos/Dialog.java
+++ Aldonza/src/main/java/com/aldonza/vinos/Dialog.java
@@ -31,7 +31,7 @@ public class Dialog extends Activity {
 		InputStream input = null;
 		//Utilizaremos un try y catch para tratar posibles excepciones
 		try{
-			input = assetManager.open("Aldonza/assets/about/aboutUs.txt");
+			input = assetManager.open("src/main/assets/aboutUs.txt");
 			ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
 			byte[] bytes = new byte[4096];
 			int len = 0;
